6d9f0122faafc2442d93f81c28c0915befecf4cb
dorkalorahalasz <halaszdodo13@gmail.com>
2024-05-29 08:53:59 +0200

    turned to a SpringBootApp and still works

--- locomotionmonitor/src/main/java/com/dorkalorahalasz/locomotionmonitor/Main.java
+++ locomotionmonitor/src/main/java/com/dorkalorahalasz/locomotionmonitor/Main.java
@@ -1,7 +1,16 @@
 package com.dorkalorahalasz.locomotionmonitor;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Start SpringBoot
+        SpringApplication.run(Main.class, args);
+        log.info("Hello world!");
     }
 }
